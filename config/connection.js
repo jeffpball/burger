3c6a7884cda6078f02d10469768f9fd4046988da
jball <jeffpball@gmail.com>
2019-06-26 22:37:57 -0700

    Setup all directories and files.

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+// Dependencies
+
+var mysql = require("mysql");
+
+// MySQL DB Connection Information (remember to change this with our specific credentials)
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "olvier01",
+  database: "burgers_db"
+});
+
+// Initiate MySQL Connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+-- Inserted a set of records into the table
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Mondo Boca Burger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Zebra Burger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Pickle and Hummus Burger", false);
