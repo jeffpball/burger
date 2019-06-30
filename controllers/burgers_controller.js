eabd8f12805f893b76df3ee0b7c1fcf978b10a3b
jball <jeffpball@gmail.com>
2019-06-29 17:53:03 -0700

    Edit ajax post request.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -20,27 +20,29 @@ router.post("/api/burgers", function(req, res) {
   burger.insertOne([
     "burger_name", "devoured"
   ], [
-    req.body.burger_name, req.body.devoured
+    req.body.burger_name, 0
   ], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
+
+  console.log("The post worked")
 });
 
 router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
-
+  
   burger.updateOne({
-      devoured: req.body.devoured
+      devoured: true
     }, condition, function(result) {
       if (result.changedRows === 0) {
         // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
       }
       res.status(200).end();
-
+      
     }
   );
 });
